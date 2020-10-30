543affa9a5b998b5f7703090dce5ed6d1ec78634
kar-cloud <56315987+kar-cloud@users.noreply.github.com>
2020-10-30 17:16:26 +0530

    adding images in jsx

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ ReactDOM.render(
       <h1 className="heading" contentEditable="true" spellCheck="false">
          My Favourite Foods
       </h1>
-      <ul>
-         <li>Bacon</li>
-         <li>Jamon</li>
-         <li>Noodles</li>
-      </ul>
+      <div>
+         <img src="https://static.toiimg.com/photo/53110049.cms" alt="pizza" />
+         <img
+            src="https://kirbiecravings.com/wp-content/uploads/2018/02/garlic-noodles-61-700x680.jpg"
+            alt="noodles"
+         />
+         <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/428px-RedDot_Burger.jpg"
+            alt="burger"
+         />
+      </div>
    </div>,
    document.getElementById("root")
 );
